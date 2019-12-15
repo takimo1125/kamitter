589883d18d519912cb6646f679ce21cd7e6707d5
YuuTakimoto <yuu9701612038@gmail.com>
2019-12-15 20:03:57 +0900

    テンプレートを修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>{{ config('app.name') }}</title>
+    <meta name="description" content="TwitterのAPIを使って、自動でターゲットを見つけ出し、自動フォローや自動いいねをしたり、自動ツイートができるWEBサービスです。"/>
+    <meta name=”keywords” content=”Twitter,自動フォロー,自動アンフォロー,自動いいね、自動ツイート、神ったー”>
 
     <!-- Styles -->
     <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
