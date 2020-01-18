ce2ccce59b6a294f6ca4ddc838b5c6997c1263a4
YuuTakimoto <yuu9701612038@gmail.com>
2020-01-19 00:34:37 +0900

    from変更

--- config/mail.php
+++ config/mail.php
@@ -4,8 +4,8 @@ return [
   "host" => "smtp.mailgun.org",
   "port" => 587,
   "from" => array(
-      "address" => "from@example.com",
-      "name" => "Example"
+      "address" => "kamitter@kamitter.com",
+      "name" => "Kamitter"
   ),
   "username" => "postmaster@sandboxc5fae876a625413db54647b1d32422c7.mailgun.org",
   "password" => "3b57021227d9db871a5d4beb9dc3e015-5645b1f9-b23286e3",
